54069e348bfc4d45bd07f1fbaef382fca981c525
jakekasan <jake.kasan@gmail.com>
2019-12-26 13:07:41 +0100

    Rename main.py to setup.py

--- main.py
+++
@@ -1,4 +0,0 @@
-import socket
-
-if __name__ == "__main__":
-    print("Running as main")
---
+++ setup.py
@@ -0,0 +1,13 @@
+"""
+    Placeholder setup.py
+"""
+
+from setuptools import setup
+
+setup(
+    name = "tor-with-python",
+    version = "0.0.1",
+    author = "Jake",
+    description = "A bare-bones Tor client and server.",
+    python_requires = ">=3.6"
+)
